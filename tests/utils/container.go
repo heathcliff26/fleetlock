883fafa4edd9e4d89af0e84e3ef0b65597c3357e
Heathcliff <heathcliff@heathcliff.eu>
2024-04-26 16:37:10 +0200

    unit-tests: Add utility to run container commands for tests
    Add utility to run container commands during tests when a runtime is available.
    
    Add failover test for redis loadbalancer using keydb.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

---
+++ tests/utils/container.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+var (
+	containerRuntime string
+	initialized      = false
+)
+
+func findContainerRuntime() string {
+	for _, cmd := range []string{"docker", "podman"} {
+		path, err := exec.LookPath(cmd)
+		if err != nil {
+			continue
+		}
+		err = exec.Command(path, "ps").Run()
+		if err == nil {
+			fmt.Printf("Found container runtime %s, path=%s\n", cmd, path)
+			return path
+		}
+	}
+	fmt.Println("Did not find any container runtimes")
+	return ""
+}
+
+func HasContainerRuntimer() bool {
+	if !initialized {
+		containerRuntime = findContainerRuntime()
+		initialized = true
+	}
+	return containerRuntime != ""
+}
+
+func ExecCRI(args ...string) error {
+	if !initialized {
+		containerRuntime = findContainerRuntime()
+		initialized = true
+	}
+	return exec.Command(containerRuntime, args...).Run()
+}
