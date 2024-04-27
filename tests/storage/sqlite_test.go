d2fe314eda0ddb45d18db3789b575de9f539d68b
Heathcliff <heathcliff@heathcliff.eu>
2024-04-27 12:18:15 +0200

    storage/sqlite3: Run test databases in memory
    Run sqlite databases in memory only to avoid leaked files after tests, as well
    as problems when running on nfs/cifs/smb storage.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- tests/storage/sqlite_test.go
+++ tests/storage/sqlite_test.go
@@ -1,7 +1,6 @@
 package storage
 
 import (
-	"os"
 	"testing"
 
 	"github.com/heathcliff26/fleetlock/pkg/lock-manager/storage/sql"
@@ -9,18 +8,12 @@ import (
 
 func TestSQLiteBackend(t *testing.T) {
 	cfg := sql.SQLiteConfig{
-		File: "test.db",
+		File: "file:test.db?mode=memory",
 	}
 	storage, err := sql.NewSQLiteBackend(&cfg)
 	if err != nil {
 		t.Fatalf("Failed to create storage backend: %v", err)
 	}
-	t.Cleanup(func() {
-		err = os.Remove("test.db")
-		if err != nil {
-			t.Logf("Failed to cleanup sqlite database file: %v", err)
-		}
-	})
 
 	RunLockManagerTestsuiteWithStorage(t, storage)
 }
