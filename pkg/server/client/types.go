efc2857a2657aef004c42281ab1f354cc995d75a
Heathcliff <heathcliff@heathcliff.eu>
2024-08-08 21:29:10 +0200

    server: Create a client package for imports
    Create a client package to help clients create fleetlock requests.
    The package should not depend on anything, to ensure that the import remains
    small, as the server package imports are huge.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

---
+++ pkg/server/client/types.go
@@ -0,0 +1,15 @@
+package client
+
+type FleetLockRequest struct {
+	Client FleetLockRequestClient `json:"client_params"`
+}
+
+type FleetLockRequestClient struct {
+	ID    string `json:"id"`
+	Group string `json:"group"`
+}
+
+type FleetLockResponse struct {
+	Kind  string `json:"kind"`
+	Value string `json:"value"`
+}
--- pkg/server/responses.go
+++ pkg/server/responses.go
@@ -1,43 +1,45 @@
 package server
 
+import "github.com/heathcliff26/fleetlock/pkg/server/client"
+
 var (
-	msgNotFound = FleetLockResponse{
+	msgNotFound = client.FleetLockResponse{
 		Kind:  "not_found",
 		Value: "The requested url is not found on this server",
 	}
-	msgWrongMethod = FleetLockResponse{
+	msgWrongMethod = client.FleetLockResponse{
 		Kind:  "bad_request",
 		Value: "Only accepts POST request",
 	}
-	msgMissingFleetLockHeader = FleetLockResponse{
+	msgMissingFleetLockHeader = client.FleetLockResponse{
 		Kind:  "missing_fleetlock_header",
 		Value: "The header fleet-lock-protocol must be set to true",
 	}
-	msgRequestParseFailed = FleetLockResponse{
+	msgRequestParseFailed = client.FleetLockResponse{
 		Kind:  "bad_request",
 		Value: "The request json could not be parsed",
 	}
-	msgInvalidGroupValue = FleetLockResponse{
+	msgInvalidGroupValue = client.FleetLockResponse{
 		Kind:  "bad_request",
 		Value: "The value of group is invalid or empty. It must conform to \"" + groupValidationPattern + "\"",
 	}
-	msgEmptyID = FleetLockResponse{
+	msgEmptyID = client.FleetLockResponse{
 		Kind:  "bad_request",
 		Value: "The value of id is empty",
 	}
-	msgUnexpectedError = FleetLockResponse{
+	msgUnexpectedError = client.FleetLockResponse{
 		Kind:  "error",
 		Value: "An unexpected error occured",
 	}
-	msgSuccess = FleetLockResponse{
+	msgSuccess = client.FleetLockResponse{
 		Kind:  "success",
 		Value: "The operation was succesfull",
 	}
-	msgSlotsFull = FleetLockResponse{
+	msgSlotsFull = client.FleetLockResponse{
 		Kind:  "all_slots_full",
 		Value: "Could not reserve a slot as all slots in the group are currently locked already",
 	}
-	msgWaitingForNodeDrain = FleetLockResponse{
+	msgWaitingForNodeDrain = client.FleetLockResponse{
 		Kind:  "waiting_for_node_drain",
 		Value: "The Slot has been reserved, but the node is not yet drained",
 	}
