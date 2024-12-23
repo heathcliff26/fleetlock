0ebaa68e489568c6c836342a2740f43cd389a401
Heathcliff <heathcliff@heathcliff.eu>
2024-12-23 15:58:35 +0100

    Add new fleetctl binary
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

---
+++ pkg/fleetctl/root_test.go
@@ -0,0 +1,13 @@
+package fleetctl
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewRootCommand(t *testing.T) {
+	cmd := NewRootCommand()
+
+	assert.Equal(t, Name, cmd.Use)
+}
